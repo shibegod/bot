c7f4a3e30983de31e414a5087cd0529b1075b12d
shibegod <31591898+shibegod@users.noreply.github.com>
2017-09-30 12:49:46 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-
+const prefix = "*"
 var bot = new Discord.Client();
 
 bot.on("ready", () => {
@@ -9,5 +9,23 @@ bot.on("ready", () => {
 bot.on('guildMemberAdd',  (member) => {
     member.guild.channels.get("343037231734456322").send("Hey dumbass welcome to this toxic cancerous gay fucking discord server hope you don't have fun bitch. Eat a dick " + member);
     });
+
+bot.on("message", message => {
+      if (message.author.bot) return;
+      if(!message.content.startsWith(prefix)) return;
+
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  console.log(command);
+    
+if (message.content.startsWith(prefix + "setgame")) {
+    if (message.author.id == "251938340671062036") {
+        let argsresult = args.join(' ')
+        bot.user.setGame(argsresult)
+        message.channel.send('game has been set')
+    }
+}
     
     bot.login(process.env.BOT_TOKEN); 
