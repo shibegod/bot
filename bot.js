241f62bbd230d92994bfbabbcbcf55ecacab27e2
shibegod <31591898+shibegod@users.noreply.github.com>
2017-09-30 11:35:08 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ bot.on("ready", () => {
 });
 
 bot.on('guildMemberAdd',  (member) => {
-    member.guild.channels.get("352990046942396420").send("Welcome to the greatest discord server of all time!, " + member);
+    member.guild.channels.get("343037231734456322").send("Welcome to the greatest discord server of all time!, " + member);
     });
     
     bot.login(process.env.BOT_TOKEN); 
